7c7519a6f761f84a5e31127c1a64ea7022caff98
Shinder Lin <shinder.lin@gmail.com>
2020-09-22 12:02:02 +0800

    charset 2

--- src/http_server.js
+++ src/http_server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
     res.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type': 'text/plain; charset=utf-8'
     });
     res.end(`
         <meta charset="UTF-8">

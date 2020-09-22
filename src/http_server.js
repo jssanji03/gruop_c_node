afd55e70a64b8e7216aff9f5c895a8464448731c
Shinder Lin <shinder.lin@gmail.com>
2020-09-22 11:59:47 +0800

    charset

--- src/http_server.js
+++ src/http_server.js
@@ -2,10 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
     res.writeHead(200, {
-        'Content-Type': 'text/plain'
+        'Content-Type': 'text/html'
     });
     res.end(`
-        <h2>Hello</h2>
+        <meta charset="UTF-8">
+        <h2>Hello 您好</h2>
         <p>${req.url}</p>
     `);
 });

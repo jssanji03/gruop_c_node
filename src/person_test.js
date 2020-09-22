9badf24d4f589a701a29db4f7638ec382c854917
Shinder Lin <shinder.lin@gmail.com>
2020-09-22 11:25:50 +0800

    require: destructuring

--- src/person_test.js
+++ src/person_test.js
@@ -1,4 +1,4 @@
-const { Person, f1 } = require('./person');
+const { Person, f1: func } = require('./person');
 const my = require('./person');
 
 const p1 = new Person('David', 23);
@@ -7,7 +7,7 @@ console.log(p1);
 console.log(''+p1);
 p1.name = 'Bill';
 console.log(p1.toJSON());
-console.log(f1(6));
+console.log(func(6));
 
 console.log(Person === my.Person);
 

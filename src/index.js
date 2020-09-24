6d473fdb0e574cb811c3be6e4c55ed02af33d1ea
Shinder Lin <shinder.lin@gmail.com>
2020-09-24 10:01:33 +0800

    404 page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res)=>{
     res.send('<h2>Hola </h2>');
 });
 
+app.use((req, res)=>{
+    res
+        .type('text/plain')
+        .status(404)
+        .send('找不到網頁');
+});
+
 app.listen(3000, ()=>{
     console.log('伺服器已啟動...');
 })

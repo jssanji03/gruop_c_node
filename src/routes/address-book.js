6a2a9e0f4f62a5fa5c0e9d342efa94fbdb15f4f2
Shinder Lin <shinder.lin@gmail.com>
2020-09-28 15:53:52 +0800

    列表 api

--- src/routes/address-book.js
+++ src/routes/address-book.js
@@ -26,16 +26,34 @@ router.get('/', (req, res)=>{
 
 router.get('/api', async (req, res)=>{
     const output = {
-        page: 1,
-        perPage: 5,
+        page: 0,
+        perPage: 10,
         totalRows: 0,
-        totalPage: 0,
+        totalPages: 0,
         rows: []
     };
 
-    const [ [ { totalRows } ] ] = await db.query("SELECT COUNT(1) totalRows FROM address_book");
+    const [[{ totalRows }]] = await db.query("SELECT COUNT(1) totalRows FROM address_book");
+    if(totalRows>0){
+        let page = parseInt(req.query.page) || 1;
+        output.totalRows = totalRows;
+        output.totalPages = Math.ceil(totalRows/output.perPage);
 
-    res.json(totalRows);
+        if(page < 1) {
+            output.page = 1;
+        } else if(page > output.totalPages) {
+            output.page = output.totalPages;
+        } else {
+            output.page = page;
+        }
+
+        let sql = `SELECT * FROM address_book ORDER BY sid DESC LIMIT ${(output.page-1)*output.perPage}, ${output.perPage}`;
+
+        const [r2] = await db.query(sql);
+        output.rows = r2;
+    }
+
+    res.json(output);
 
 });
 

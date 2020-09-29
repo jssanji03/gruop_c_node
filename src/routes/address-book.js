f3cf2df5e4d3a7d30e4635af3edd47ac3b1b9662
Shinder Lin <shinder.lin@gmail.com>
2020-09-29 13:28:56 +0800

    pagination btns

--- src/routes/address-book.js
+++ src/routes/address-book.js
@@ -11,7 +11,7 @@ router.get('/', (req, res)=>{
 async function getListData (req) {
     const output = {
         page: 0,
-        perPage: 5,
+        perPage: 10,
         totalRows: 0,
         totalPages: 0,
         rows: [],
@@ -33,28 +33,53 @@ async function getListData (req) {
         }
 
         // 處理頁碼按鈕
-        if(output.totalPages <= 7){
-            for(let i=1; i<=output.totalPages; i++){
-                output.pages.push(i);
-            }
-        } else {
-            const fAr = [], bAr = [];
-            // 從前面開始數
-            for(let i=output.page-3; i<=output.totalPages; i++){
-                if(i>=1){
-                    fAr.push(i);
+        (function(page, totalPages, prevNum){
+            let pages = [];
+            if(totalPages <= prevNum*2+1){
+                for(let i=1; i<=totalPages; i++){
+                    pages.push(i);
                 }
-                if(fAr.length >= 7) break;
-            }
-            // 從後面開始數
-            for(let i=output.page+3; i>=1; i--){
-                if(i<=output.totalPages){
-                    bAr.unshift(i);
+            } else {
+                const fAr = [], bAr = [];
+                // 從前面開始數
+                for(let i=page-prevNum; i<=totalPages; i++){
+                    if(i>=1){
+                        fAr.push(i);
+                    }
+                    if(fAr.length >= prevNum*2+1) break;
                 }
-                if(bAr.length >= 7) break;
+                // 從後面開始數
+                for(let i=page+prevNum; i>=1; i--){
+                    if(i<=totalPages){
+                        bAr.unshift(i);
+                    }
+                    if(bAr.length >= prevNum*2+1) break;
+                }
+                pages = fAr.length > bAr.length ? fAr : bAr;
             }
-            output.pages = fAr.length > bAr.length ? fAr : bAr;
-        }
+            output.pages = pages;
+        })(page, output.totalPages, 3);
+
+        // 處理頁碼按鈕 2
+        (function(page, totalPages, prevNum){
+            let beginPage, endPage;
+            if(totalPages <= prevNum*2+1){
+                beginPage = 1;
+                endPage = totalPages;
+            } else if(page-1 < prevNum) {
+                beginPage = 1;
+                endPage = prevNum*2+1;
+            } else if(totalPages-page < prevNum) {
+                beginPage = totalPages-(prevNum*2+1);
+                endPage = totalPages;
+            } else {
+                beginPage = page-prevNum;
+                endPage = page+prevNum;
+            }
+            output.beginPage = beginPage;
+            output.endPage = endPage;
+        })(page, output.totalPages, 3);
+
 
         let sql = `SELECT * FROM address_book ORDER BY sid DESC LIMIT ${(output.page-1)*output.perPage}, ${output.perPage}`;
 
